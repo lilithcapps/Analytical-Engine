e46b1607731b20e71d5e5e0d9b32918d2528bca6
Lilith Capps <lilithjcapps@gmail.com>
2025-05-25 06:53:44 +0100

    Refactor process into createOperationChain. Add applyParameters

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE MultiWayIf        #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ParallelListComp  #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+{-# OPTIONS_GHC -Wno-unused-local-binds #-}
 
-import           Data.Char         (intToDigit)
-import qualified Data.Text.Lazy    as T
-import qualified Data.Text.Lazy.IO as IO
+import           Control.Lens             (ix, (&), (.~))
+import           Control.Monad.State.Lazy
+import           Data.Char                (intToDigit)
+import qualified Data.Text.Lazy           as T
+import qualified Data.Text.Lazy.IO        as IO
 
-type VariablePunchCard = NumberPunchCard
+type DistributivePunchCard = NumberPunchCard
 type NumericPunchCard = NumberPunchCard
 type NumberPunchCard = PunchCard
 
@@ -15,178 +18,213 @@ type OperationPunchCard = PunchCard
 type PunchCard = [PunchCardLine]
 type PunchCardLine = String
 
+type UnboundOperation = Either MathsOperation UnboundVariableOperation
+type PairedOperation = Either MathsOperation (Int, UnboundVariableOperation)
 type Operation = Either MathsOperation VariableOperation
-type RightPairedOperation = Either MathsOperation (Int, VariableOperation)
-type TuplePairedOperation = ([Int], Either MathsOperation VariableOperation)
 
-type MathsOperation = Either ArithmeticOperation DivisionOperation
-type VariableOperation = Either StoreOperation MoveOperation
+type MillValue = (Integer, Integer)
+type StoreValue = [Integer]
+type Primacy = Bool
 
-type ArithmeticOperation = Integer -> Integer -> Integer
-type DivisionOperation = Integer -> Integer -> (Integer, Integer)
-data StoreOperation = LoadPreserve | LoadZero | Store | StorePrimed
-type MoveOperation = Int -> Int
+type EngineState = (MillValue, MathsOperation, StoreValue, Primacy)
 
+data MathsOperation = Add | Subtract | Multiply | Divide
+  deriving Show
+data UnboundVariableOperation =
+  UnboundSupplyRetaining
+  | UnboundSupplyZeroing
+  | UnboundStore
+  | UnboundStorePrimed
+  | UnboundForwards
+  | UnboundBackwards
+  deriving Show
+data VariableOperation =
+  SupplyRetaining Int
+  | SupplyZeroing Int
+  | Store Int
+  | StorePrimed Int
+  | Forwards Int
+  | Backwards Int
+  deriving Show
 
-process operationCards numericCards variableCards = do
-  let numbers :: [Integer]
-      numbers = map parseNumeric numericCards
-  let variables :: [Int]
-      variables = map (read . parseCardNumber) variableCards
-
-  let operations = map parseOperation operationCards
-
-  return (test' operations numbers variables)
-  --return process' (Left (+)) [] operations [] numbers [] variables
-
-test' :: [Operation] -> [Integer] -> [Int] -> m a2
-test' operations numbers variables = do
-
-  -- split
-  -- then pair, using list of vars passed in, and returning the tail when its a real value
-  -- then parallel iterate arith n ldst
-  -- maybe have one of them going backwards ?
-
-  let fullyPairedOps :: [TuplePairedOperation]
-      -- the magic: 0 gets dropped when we actually first
-      -- need a val, leaving us with a tail whose head is the value we want
-      -- (+) is our starting value, meaning we don't have to set it anywhere
-      fullyPairedOps = (0:variables, Left . Left $ (+)) : [
-        (case op of
-          Left l  ->        (fst . last $ fullyPairedOps, Left l)
-          Right r -> (tail . fst . last $ fullyPairedOps, Right r)
-          )| op <- operations]
-
-
-  -- this is lefties alone and righties with their var
-  let opsWVars :: [RightPairedOperation]
-      opsWVars = [ (case op of
-        (_, Left l)    -> Left l
-        (v:_, Right r) -> Right (v, r)
-        ([], Right _)  -> error "variables terminated early")
-        | op <- fullyPairedOps]
-
-  -- now do skip back and forth processing
-  let singleStream :: [Either MathsOperation (Int, StoreOperation)]
-      singleStream = concat [ case op of
-        (idx, Right (var, Right rr)) -> parseMoveOp idx (rr var)
-        (_, Right (var, Left lr))    -> [Right (var, lr)]
-        (_, Left l)                  -> [Left l]
-          | op <- zip [0..] opsWVars]
-          where
-            parseMoveOp :: Int -> Int -> [Either MathsOperation (Int, StoreOperation)]
-            parseMoveOp idx op =
-              if  | op > 0 -> [singleStream !! (idx+index) |index <- [1..op]]
-                  | op < 0 -> [opsWVars !! (idx+index) | index <- [op,(op-1)..(-1)]]
-                  | True   -> error "variable cannot be 0 for a movement operation"
-
-
-
-
-
-  -- then do full pass of process
-
-  -- .. numbers? ... stored vars? .... the storeeeee?
-
-  let arithmeticOps :: [MathsOperation]
-      arithmeticOps = Left (+) : [ (case op of
-        Left l  -> l
-        Right _ -> last arithmeticOps)
-        | op <- operations]
-
-  let loadStoreOps = -1 : [ (case op of
-        Left _ -> -1
-        Right r -> case r of
-          Left rl  -> undefined
-          Right rr -> undefined {- do load store stuff???? -})
-        | op <- operations]
-
-  undefined
-
-{-
-process' :: Either ArithmeticOperation DivisionOperation -> [Operation] -> [Operation] -> [Integer] -> [Integer] -> [Int] -> [Int] -> a
-process' _ _ [] _ _ _ _ = 0
-process' _ _ (_:_) _ [] _ _ = 0
-process' _ _ (_:_) _ (_:_) _ [] = 0
--- prevs are LIFO lists i.e. the first item is the var we just processed
-process' selectedOp prevOps nextOps@(op:ops) prevNums nextNums@(num:nums) prevVars nextVars@(var:vars)
- = case op of
-    Left l -> process' l prevOps nextOps prevNums nextNums prevVars nextVars
-    Right r -> case r of
-      -- Store Op
-      Left rl  -> case rl of
-        0 -> undefined -- loadPreservelogic
-        1 -> undefined -- loadZerologic
-        2 -> undefined -- storelogic
-        3 -> undefined -- storePrimedlogic
-        _ -> error "bad value"
-      -- Movement Op
-      Right rr ->
-        if | result > 0 -> process' selectedOp (reverse (take (var + 1) nextOps) ++ prevOps) (drop (var + 1) nextOps) prevNums nextNums prevVars nextVars
-           | result < 0 -> process' selectedOp (drop (var - 1) prevOps) (reverse (take (var - 1) prevOps) ++ nextOps) prevNums nextNums prevVars nextVars
-           | True -> error "bad movement var"
-           where
-            result = rr var
--}
-
-parseOperation :: OperationPunchCard -> Operation
-parseOperation card =
-  if | card == addCard          -> Left  . Left  $ (+)
-     | card == subtractCard     -> Left  . Left  $ (-)
-     | card == multiplyCard     -> Left  . Left  $ (*)
-     | card == divideCard       -> Left  . Right $ div'
-     | card == loadPreserveCard -> Right . Left  $ LoadPreserve
-     | card == loadZeroCard     -> Right . Left  $ LoadZero
-     | card == storeCard        -> Right . Left  $ Store
-     | card == storePrimedCard  -> Right . Left  $ StorePrimed
-     | card == forwardsCard     -> Right . Right $ (0+)
-     | card == backwardsCard    -> Right . Right $ (0-)
-     | True                     -> error "unknown operation card"
+applyParameters :: [Operation] -> [Integer] -> [StoreValue]
+applyParameters ops params = states
   where
-    div' a b = (a `div` b, a `mod` b)
-    addCard          = ["* ", "  ", "  ", "  ", "  "]
-    subtractCard     = [" *", "  ", "  ", "  ", "  "]
-    multiplyCard     = ["  ", "* ", "  ", "  ", "  "]
-    divideCard       = ["  ", " *", "  ", "  ", "  "]
-    loadPreserveCard = ["  ", "  ", "* ", "  ", "  "]
-    loadZeroCard     = ["  ", "  ", " *", "  ", "  "]
-    storeCard        = ["  ", "  ", "  ", "* ", "  "]
-    storePrimedCard  = ["  ", "  ", "  ", " *", "  "]
-    forwardsCard     = ["* ", "  ", "  ", "  ", "* "]
-    backwardsCard    = [" *", "  ", "  ", "  ", "* "]
+    store = params ++ [0,0..] :: StoreValue
+    mill = (0, 0) :: MillValue
 
+    states :: [StoreValue]
+    states = evalState (mapM engine ops) (mill, Add, store, False)
+      where
+        engine :: Operation -> State EngineState StoreValue
+        engine op = get
+          >>= put . doOperation op
+          >> get
+          >>= (\(_, _, s, _) -> return s)
 
-parseNumeric :: NumericPunchCard -> Integer
-parseNumeric [] = error "empty punchcard"
-parseNumeric card = case head . head $ card of
-  '*' -> read $ '-' : parseCardNumber (tail <$> card :: NumericPunchCard) -- remove the first char from each line of the card
-  ' ' -> read $ '+' : parseCardNumber (tail <$> card :: NumericPunchCard) -- this removes the leading space in numeric cards
-  _   -> error "badly formatted punchcard"
+    doOperation :: Operation -> EngineState -> EngineState
+    doOperation op s@((a, b), _, store, primed) = case (op, primed) of
+      (Right (SupplyRetaining n), True) -> doArithmetic . doDistributive (SupplyRetaining n) $ s
+      (Right (SupplyZeroing n), True) -> doArithmetic . doDistributive (SupplyRetaining n) $ s
+      (Right r, _) -> doDistributive r s
+      (Left l, _) -> ((a, b), l, store, primed)
+      where
+        doDistributive :: VariableOperation -> EngineState -> EngineState
+        doDistributive op s@((a, b), mathOp, store, primed) = case op of
+          SupplyRetaining n -> if primed then ((store !! n, b), mathOp, store, not primed) else ((a, store !! n), mathOp, store, not primed)
+          SupplyZeroing n   -> if primed then ((store !! n, b), mathOp, store & ix n .~ 0, not primed) else ((a, store !! n), mathOp, store & ix n .~ 0, not primed)
+          Store n           -> ((0, b), mathOp, store & ix n .~ a, primed)
+          StorePrimed n     -> ((a, 0), mathOp, store & ix n .~ b, primed)
+          Forwards _        -> s
+          Backwards _       -> s
 
-parseCardNumber :: NumberPunchCard -> String
-parseCardNumber cardLines =
-  [ intToDigit (col + 1)
-    | row <- [0..length . head $ cardLines]
-    , col <- [0..length cardLines]
-    , let val = cardLines !! row !! col
-    , val == '*'
-  ]
+        doArithmetic :: EngineState -> EngineState
+        doArithmetic ((a, b), op, store, primed) =
+          let mill = case op of
+                Add      -> (a + b, 0)
+                Subtract -> (a - b, 0)
+                Multiply -> (a * b, 0)
+                Divide   -> (a `div` b, a `mod` b)
+          in (mill, op, store, primed)
 
-readCard :: FilePath -> IO T.Text
-readCard loc = IO.readFile loc
+    arithmeticOps :: [MathsOperation]
+    arithmeticOps = [(case op of
+      Left l  -> l
+      Right _ -> last arithmeticOps)
+      | op <- ops]
 
+    distributiveOps :: [Either Int VariableOperation]
+    distributiveOps = [ (case op of
+        Left _  -> Left 0
+        Right r -> Right r)
+        | op <- ops]
 
+createOperationChain :: [UnboundOperation] -> [Int] -> [Operation]
+createOperationChain us vs = operationChain
+  where
+  -- this is lefties alone and righties as data with variable
+  operations :: [Operation]
+  operations = fmap bindVariableOperation <$> evalState (mapM pairVars us) vs
+    where
+      pairVars :: UnboundOperation -> State [Int] PairedOperation
+      pairVars (Left l) =
+        get >>= \vars -> case vars of
+            [] -> error "ran out of vars :("
+            _  -> return (vars, Left l)
+            >>= \(v, e) -> put v >> return e
+      pairVars (Right r) =
+        get >>= \vars -> case vars of
+            []     -> error "ran out of vars :("
+            v:vs -> return (vs, Right (v, r))
+          >>= \(v, e) -> put v >> return e
+
+      bindVariableOperation :: (Int, UnboundVariableOperation) -> VariableOperation
+      bindVariableOperation (n, UnboundSupplyRetaining) = SupplyRetaining n
+      bindVariableOperation (n, UnboundSupplyZeroing)   = SupplyZeroing n
+      bindVariableOperation (n, UnboundStore)           = Store n
+      bindVariableOperation (n, UnboundStorePrimed)     = StorePrimed n
+      bindVariableOperation (n, UnboundForwards)        = Forwards n
+      bindVariableOperation (n, UnboundBackwards)       = Backwards n
+
+  -- create infinite list of operations
+  operationChain = (\(x, _, _) -> x) <$> go
+    where
+      go :: [(Operation, [Operation], [Operation])]
+      go = (head operations, [], tail operations) : more go
+        where
+          more :: [(Operation, [Operation], [Operation])] -> [(Operation, [Operation], [Operation])]
+          more ((c@(Right (Forwards n)), ps, ns):_) =
+            let nextTup = ((!! max 0 n) ns, reverse (take n ns) ++ (c : ps), tail . drop n $ ns) : more nextTup
+            in nextTup
+          more ((c@(Right (Backwards n)), ps, ns):_) =
+            let newPrevs = c : ps in
+            let nextTup = ((!! max 0 (n-1)) newPrevs, drop n newPrevs, reverse (take (n-1) newPrevs) ++ ns) : more nextTup
+            in nextTup
+          more ((c, ps, n:ns):_) =
+            let nextTup = (n, c:ps, ns) : more nextTup
+            in nextTup
+          more _ = []
+
+main :: IO ()
 main = do
-  arithmeticString <- readCard "default/operations.pc"
-  numericFile <- readCard "default/numbers.pc"
-  loadStoreFile <- readCard "default/loadStore.pc"
+  arithmeticString <- readCard "programs/default/operations.pc"
+  numericFile <- readCard "programs/default/numbers.pc"
+  distributiveFile <- readCard "programs/default/loadStore.pc"
 
-  let operations :: [OperationPunchCard]
-      operations = map (lines . T.unpack) $ T.splitOn "-\r\n" arithmeticString
-  let numbers :: [NumericPunchCard]
-      numbers = map (lines . T.unpack) $ T.splitOn "-\r\n" numericFile
-  let variables :: [VariablePunchCard]
-      variables = map (lines . T.unpack) $ T.splitOn "-\r\n" loadStoreFile
+  let operations :: [UnboundOperation]
+      operations = parseOperation . lines . T.unpack <$> T.splitOn "-\r\n" arithmeticString
+  let distributive :: [Int]
+      distributive = parseVariable . lines . T.unpack <$> T.splitOn "-\r\n" distributiveFile
+  let numbers :: [Integer]
+      numbers = parseNumeric . lines . T.unpack <$> T.splitOn "-\r\n" numericFile
 
-  return undefined
-  --return $ process operations numbers variables
+  let storeValues = applyParameters (createOperationChain operations distributive) numbers
+
+  let inputOps = [Left Add, Right UnboundForwards, Left Divide, Right UnboundBackwards]
+  let inputVars = [1, 3]
+  let inputParams = [0, 0]
+
+  let opChain = createOperationChain inputOps inputVars
+  let computedValues = applyParameters opChain inputParams
+
+  putStrLn . (++) "Initial Operations: " $ show . prettyPrintEither $ inputOps
+  putStrLn $ "Initial Variables: " ++ show inputVars
+  putStrLn $ "Initial Paramters: " ++ show inputParams
+
+  print . prettyPrintEither . take 10 $ opChain
+  print . take 10 $ computedValues
+  return ()
+  where
+    readCard :: FilePath -> IO T.Text
+    readCard = IO.readFile
+
+    prettyPrintEither :: (Monad m, Show a, Show b) => m (Either a b) -> m String
+    prettyPrintEither = (either show show <$>)
+
+    parseOperation :: OperationPunchCard -> UnboundOperation
+    parseOperation card =
+      if | card == addCard           -> Left Add
+          | card == subtractCard     -> Left Subtract
+          | card == multiplyCard     -> Left Multiply
+          | card == divideCard       -> Left Divide
+          | card == loadPreserveCard -> Right UnboundSupplyRetaining
+          | card == loadZeroCard     -> Right UnboundSupplyZeroing
+          | card == storeCard        -> Right UnboundStore
+          | card == storePrimedCard  -> Right UnboundStorePrimed
+          | card == forwardsCard     -> Right UnboundForwards
+          | card == backwardsCard    -> Right UnboundBackwards
+          | True                     -> error "unknown operation card"
+      where
+        addCard          = ["* ", "  ", "  ", "  ", "  "]
+        subtractCard     = [" *", "  ", "  ", "  ", "  "]
+        multiplyCard     = ["  ", "* ", "  ", "  ", "  "]
+        divideCard       = ["  ", " *", "  ", "  ", "  "]
+        loadPreserveCard = ["  ", "  ", "* ", "  ", "  "]
+        loadZeroCard     = ["  ", "  ", " *", "  ", "  "]
+        storeCard        = ["  ", "  ", "  ", "* ", "  "]
+        storePrimedCard  = ["  ", "  ", "  ", " *", "  "]
+        forwardsCard     = ["* ", "  ", "  ", "  ", "* "]
+        backwardsCard    = [" *", "  ", "  ", "  ", "* "]
+
+    parseVariable :: DistributivePunchCard -> Int
+    parseVariable = read . parseCardNumber
+
+    parseNumeric :: NumericPunchCard -> Integer
+    parseNumeric []   = error "empty punchcard"
+      -- remove the first char from each line of the card
+    parseNumeric card = read $ parseSign card : parseCardNumber (tail <$> card) -- this removes the leading space in numeric cards
+
+    parseSign [] = error "empty punchcard"
+    parseSign card = case head . head $ card of
+      '*' -> '-'
+      ' ' -> '+'
+      _   -> error "badly formatted punchcard"
+
+    parseCardNumber :: NumberPunchCard -> String
+    parseCardNumber cardLines =
+      [ intToDigit (col + 1)
+        | row <- [0..length . head $ cardLines]
+        , col <- [0..length cardLines]
+        , let val = cardLines !! row !! col
+        , val == '*'
+      ]
