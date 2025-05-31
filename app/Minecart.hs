4f2bbf445f2a09e9a5c92920cb745ee33dd18590
Lilith Capps <lilithjcapps@gmail.com>
2025-05-31 01:25:03 +0100

    Added Minecart to make list traversal easier..

---
+++ app/Minecart.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs                  #-}
+{-# LANGUAGE StandaloneDeriving     #-}
+module Minecart where
+
+data Minecart c ps ns where
+  Minecart ::  a -> [a] -> [a] -> Minecart a [a] [a]
+
+deriving instance Show a => Show (Minecart a b c)
+
+shorten :: Int -> Minecart a [a] [a] -> Maybe (Minecart a [a] [a])
+shorten n (Minecart c ps ns) = Just (Minecart c (take n ps) (take n ns))
+
+-- (<$>) :: (a -> b) -> Minecart a [a] [a] -> Minecart b [b] [b]
+-- (<$>) f (Minecart c ps ns) = Minecart (f c) (fmap f ps) (fmap f ns)
+-- fmap :: (a -> b) -> m (Minecart a [a] [a]) -> m (Minecart b [b] [b])
+-- fmap f mc = mc >>= (\(Minecart c ps ns)
+--             -> return (Minecart (f c) (Prelude.fmap f ps) (Prelude.fmap f ns)))
+
+class (MonadFail m) => MonadMinecart m where
+
+  mount :: [a] -> m (Minecart a [a] [a])
+  mount (a:as) = return (Minecart a [] as)
+  mount []     = fail "no railz"
+
+  dismount :: Minecart a [a] [a] -> m a
+  dismount (Minecart a _ _) = return a
+
+  next :: Minecart a [a] [a] -> m (Minecart a [a] [a])
+  next (Minecart c ps (n:ns)) = return (Minecart n (c:ps) ns)
+  next (Minecart _ _ [])      = fail "outta railz"
+
+  prev :: Minecart a [a] [a] -> m (Minecart a [a] [a])
+  prev (Minecart c (p:ps) ns) = return (Minecart p ps (c:ns))
+  prev (Minecart _ [] _)      = fail "outta railz"
+
+
+  forwardsN :: Int -> Minecart a [a] [a] -> m (Minecart a [a] [a])
+  forwardsN n mc
+    | n > 0 = next mc >>= forwardsN (n-1)
+    | otherwise = return mc
+
+  backwardsN :: Int -> Minecart a [a] [a] -> m (Minecart a [a] [a])
+  backwardsN n mc
+    | n > 0 = prev mc >>= backwardsN (n-1)
+    | otherwise = return mc
+
+instance MonadMinecart Maybe
